5dd7e4432e14c3eb1dc5aba5b338adcfbf687277
KUMAR SWAMI <checharprajakta1660@gmail.com>
2025-07-23 16:03:08 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -111,31 +111,27 @@
 	
 	/* ..............................................
     Countdown Clock
-    ................................................. */
+	................................................. */
 	function makeTimer() {
-		var endTime = new Date("27 December 2025 23:59:59 GMT+01:00");			
-		endTime = (Date.parse(endTime) / 1000);
+	const endTime = new Date("27 December 2025 23:59:59 GMT+0900");
+	const now = new Date();
 
-		var now = new Date();
-		now = (Date.parse(now) / 1000);
+	const timeLeft = Math.floor((endTime - now) / 1000);
 
-		var timeLeft = endTime - now;
+	const days = Math.floor(timeLeft / 86400);
+	const hours = Math.floor((timeLeft % 86400) / 3600);
+	const minutes = Math.floor((timeLeft % 3600) / 60);
+	const seconds = Math.floor(timeLeft % 60);
 
-		var days = Math.floor(timeLeft / 86400); 
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+	const format = (num) => (num < 10 ? "0" + num : num);
 
-		if (hours < "10") { hours = "0" + hours; }
-		if (minutes < "10") { minutes = "0" + minutes; }
-		if (seconds < "10") { seconds = "0" + seconds; }
-
-		$("#days").html(days + "<h6>Days</h6>");
-		$("#hours").html(hours + "<h6>Hrs</h6>");
-		$("#minutes").html(minutes + "<h6>Min</h6>");
-		$("#seconds").html(seconds + "<h6>Sec</h6>");
+	$("#days").html(format(days) + "<h6>Days</h6>");
+	$("#hours").html(format(hours) + "<h6>Hrs</h6>");
+	$("#minutes").html(format(minutes) + "<h6>Min</h6>");
+	$("#seconds").html(format(seconds) + "<h6>Sec</h6>");
 	}
-	setInterval(function() { makeTimer(); }, 1000);
+
+	setInterval(makeTimer, 1000);
 
 
 	
